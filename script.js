f73ffcdc2e5be16a265c7b839a266bd358b7f2a3
iftikarjahan <69801895+iftikarjahan@users.noreply.github.com>
2024-04-18 15:14:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	let res="";
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let len=characters.length;
+	for(let i=0;i<l;i++){
+		res+=characters.charAt(Math.floor(Math.random()*len))
+	}
+	return res;
 }
 
 // Do not change the code below.
